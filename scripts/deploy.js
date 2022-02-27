365b19209514b1a09035cbb2ef598a69fc5941e6
Santiago Marcano <marcanoblog@gmail.com>
2022-02-27 18:26:07 -0300

    free mint?

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,30 +3,38 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const hre = require('hardhat')
 
-async function main() {
+async function main () {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
   //
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   // await hre.run('compile');
+  const MAX_ELEMENTS = 2000
+  const BASE_TOKEN_URI =
+    'https://the-bad-yonkies-dm8dd73be-sayesito.vercel.app/api/metadata'
+  const proxyRegistryAddress = '0x58807bad0b376efc12f5ad86aac70e78ed67deae'
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const NFT = await hre.ethers.getContractFactory('NFT')
+  const nft = await NFT.deploy(
+    BASE_TOKEN_URI,
+    MAX_ELEMENTS,
+    proxyRegistryAddress
+  )
 
-  await greeter.deployed();
+  await nft.deployed()
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log('NFT deployed to:', nft.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
