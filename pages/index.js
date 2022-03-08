1d360925797da3cafe989401024ff548dfea3cc5
Santiago Marcano <marcanoblog@gmail.com>
2022-03-08 19:55:49 -0300

    casing import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Main from '../components/Main'
-import NavBar from '../components/Navbar'
+import NavBar from '../components/NavBar'
 
 function Landing () {
   return (
