43b77d1c80d67404b734cbc2b00cccecd3fcd726
Santiago Marcano <marcanoblog@gmail.com>
2022-03-12 12:51:55 -0300

    favicon, title

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel='shortcut icon' href='/favi.png' />
+        <title>Berlin Yonkies - INSPIRED BY THE BERLIN NIGHTLIFE</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
