a75964a8e10e4a65a10a3829d5253b4ad8824197
Santiago Marcano <marcanoblog@gmail.com>
2022-03-15 10:05:42 -0300

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Script from 'next/script'
+
 import '../styles/globals.css'
 
 export default function App ({ Component, pageProps }) {
@@ -7,6 +9,21 @@ export default function App ({ Component, pageProps }) {
       <Head>
         <link rel='shortcut icon' href='/favi.png' />
         <title>Berlin Yonkies - INSPIRED BY THE BERLIN NIGHTLIFE</title>
+        <Script
+          strategy='afterInteractive'
+          src={`'https://www.googletagmanager.com/gtag/js?id=G-KLRXCYVSL4'`}
+        />
+        <Script
+          id='gtag-init'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-KLRXCYVSL4');
+        `
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
