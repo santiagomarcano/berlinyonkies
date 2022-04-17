6afc24b58d861259264445314e3605a86acf73f8
Santiago Marcano <marcanoblog@gmail.com>
2022-04-16 22:52:01 -0300

    test

--- pages/api/metadata/[name].js
+++ pages/api/metadata/[name].js
@@ -1,6 +1,6 @@
+const file = JSON.parse(require('fs').readFileSync('collection/_metadata.json'))
 export default async function handler (req, res) {
-  const { name } = req.query
-  const file = JSON.parse(require('fs').readFileSync('collection/_metadata.json'))
-  const item = file.find(f => f.name === name)
-  res.status(200).json({ ...item })
+  // const { name } = req.query
+  // const item = file.find(f => f.name === name)
+  res.status(200).json({ hi: 'gola' })
 }
